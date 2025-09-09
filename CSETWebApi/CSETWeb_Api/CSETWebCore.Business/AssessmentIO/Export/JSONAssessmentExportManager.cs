bc37b5b597e1b8c79786481c0a35fc066a6b5ab2
Mostafa Abdo <mostafaxcodes@gmail.com>
2025-09-09 14:52:20 -0700

    initialize JSON Assessment Export Manager

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/AssessmentIO/Export/JSONAssessmentExportManager.cs
@@ -0,0 +1,13 @@
+//////////////////////////////// 
+// 
+//   Copyright 2025 Battelle Energy Alliance, LLC  
+// 
+// 
+////////////////////////////////
+
+namespace CSETWebCore.Business.AssessmentIO.Export
+{
+    public class JSONAssessmentExportManager
+    {
+    }
+}

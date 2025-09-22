a9d4557b4e64c5fbaad6b81ab5e1024e1b2fd611
Matt Winston <matthew.winston@inl.gov>
2025-09-22 17:11:59 -0400

    fills empty observation_Contacts array with a unselected assessment contacts

--- CSETWebNg/src/app/assessment/questions/observations/observations-general/observations-general.component.ts
+++ CSETWebNg/src/app/assessment/questions/observations/observations-general/observations-general.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ObservationDetailComponent } from '../observation-detail.component';
-import { Importance, Observation } from '../observations.model';
+import { Importance, Observation, ObservationContact } from '../observations.model';
 import { ObservationsService } from '../../../../services/observations.service';
 import { TranslocoService } from '@jsverse/transloco';
 import { LayoutService } from '../../../../services/layout.service';
@@ -57,7 +57,7 @@ export class ObservationsGeneralComponent implements OnInit {
     let obs = await firstValueFrom(this.obsSvc.getObservation(-1, observationId, -1, ''));
 
     if (!obs) {
-      obs = this.buildEmptyObservation();
+      obs = await this.buildEmptyObservation();
       // this component only builds assessment-level observations
       obs.answerLevel = false;
     }
@@ -107,7 +107,7 @@ export class ObservationsGeneralComponent implements OnInit {
   /**
    * 
    */
-  buildEmptyObservation(): Observation {
+  async buildEmptyObservation(): Promise<Observation> {
     return {
       // ACET fields
       question_Id: 0,
@@ -134,8 +134,29 @@ export class ObservationsGeneralComponent implements OnInit {
       supp_Guidance: '',
       // Shared fields
       importance: {} as Importance,
-      observation_Contacts: [],
+      observation_Contacts: await this.getContactsForEmptyObservation(),
       answerLevel: false
     };
+  }
+
+  /**
+   * Fills the empty observation_Contacts field with front-facing array
+   * of contacts assigned to the assessment, which will only be saved to
+   * the database and assigned real assessment_Contact_Ids 
+   * if the user saves the observation
+   */
+  async getContactsForEmptyObservation(): Promise<ObservationContact[]> {
+    let observationContacts = [];
+    let userContacts = (await this.assessSvc.getAssessmentContacts()).contactList;
+    userContacts.forEach(user => {
+      observationContacts.push({
+        assessment_Contact_Id: user.assessmentContactId,
+        name: user.primaryEmail + ' -- ' + user.firstName + ' ' + user.lastName,
+        observation_Id: 0,
+        selected: false
+      });
+    });
+
+    return observationContacts;
   }
 }

97a8f7123e542e8a488e67b6729f3436886d9f37
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-10-07 12:35:51 -0600

    cis comments update

--- CSETWebNg/src/app/reports/cis-commentsmarked/cis-commentsmarked.component.ts
+++ CSETWebNg/src/app/reports/cis-commentsmarked/cis-commentsmarked.component.ts
@@ -27,36 +27,42 @@ import { ReportService } from '../../services/report.service';
 import { ConfigService } from '../../services/config.service';
 import { Title, DomSanitizer } from '@angular/platform-browser';
 import { MaturityService } from '../../services/maturity.service';
+import { AssessmentDetail } from '../../models/assessment-info.model';
+import { AssessmentService } from '../../services/assessment.service';
 
 @Component({
-    selector: 'app-cis-commentsmarked',
-    templateUrl: './cis-commentsmarked.component.html',
-    styleUrls: ['../reports.scss'],
-    standalone: false
+  selector: 'app-cis-commentsmarked',
+  templateUrl: './cis-commentsmarked.component.html',
+  styleUrls: ['../reports.scss'],
+  standalone: false
 })
 export class CisCommentsmarkedComponent implements OnInit {
-  response: any = null;
+  response: AssessmentDetail;
   loading: boolean = false;
 
   constructor(
     public analysisSvc: ReportAnalysisService,
     public reportSvc: ReportService,
     public configSvc: ConfigService,
-    private titleService: Title,
     public maturitySvc: MaturityService,
-    private sanitizer: DomSanitizer
+    public assessSvc: AssessmentService
   ) { }
 
   ngOnInit(): void {
     this.loading = true;
-    this.titleService.setTitle("Comments Report - CISA CIS");
 
-    this.maturitySvc.getCommentsMarked().subscribe(
-      (r: any) => {
+    this.assessSvc.getAssessmentDetail().subscribe(
+      (r: AssessmentDetail) => {
         this.response = r;
         this.loading = false;
-      },
-      error => console.error('Comments Marked Report Error: ' + (<Error>error).message)
+      }
     );
+    // this.maturitySvc.getCommentsMarked().subscribe(
+    //   (r: any) => {
+    //     this.response = r;
+    //     this.loading = false;
+    //   },
+    //   error => console.error('Comments Marked Report Error: ' + (<Error>error).message)
+    // );
   }
 }
